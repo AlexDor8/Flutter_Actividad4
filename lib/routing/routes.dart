0412d16d490bd41d109be22a7014b7759097027a
AlexDor8 <st2022273@365.stucom.com>
2023-02-14 19:21:11 +0100

    comienzo routing

---
+++ lib/routing/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const fisrtRoute = '/firstRoute';
+  static const secondRoute = '/secondRoute';
+  static const thirdRoute = '/thirdRoute';
+  static const splash = '/splash';
+}
